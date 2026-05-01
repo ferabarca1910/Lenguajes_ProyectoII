eb11822c44eb8fdc3c5c4dd81a8d7486a8c90c2c
JoGuevara28 <106563305+JoGuevara28@users.noreply.github.com>
2026-05-01 16:08:20 -0600

    Update Storage.hs

--- Storage.hs
+++ Storage.hs
@@ -1,12 +1,21 @@
 module Storage where
 
 import Types
+import System.Directory (doesFileExist)
 
---Recibe una lista de FinanciaRecord y devuelve una acción tipo IO ()
+-- Guarda la lista de registros en un archivo de texto
+-- Se usa show para serializar la lista
 saveRecords :: [FinancialRecord] -> IO ()
 saveRecords records = writeFile "records.txt" (show records)
 
+-- Carga los registros desde el archivo
+-- Si el archivo no existe, retorna lista vacía para evitar errores
 loadRecords :: IO [FinancialRecord]
 loadRecords = do
-  content <- readFile "records.txt"
-  return (read content)
+  exists <- doesFileExist "records.txt"
+
+  if not exists
+    then return []
+    else do
+      content <- readFile "records.txt"
+      return (read content)
